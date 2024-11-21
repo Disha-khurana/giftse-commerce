937991c8104d748fa1127beb38988a9a194ce793
Disha <131692855+Disha-khurana@users.noreply.github.com>
2024-11-21 17:50:16 +0530

    updated flower page

--- frontend/src/app/flowers/page.js
+++ frontend/src/app/flowers/page.js
@@ -111,20 +111,84 @@ function Page() {
             },
         ],
 
-        surprise:[
+        surprise: [
             {
-                img:'/Images/flowers/surprise/Him.webp',
-                link:'/him'
+                img: '/Images/flowers/surprise/Him.webp',
+                link: '/him'
             },
             {
-                img:'/Images/flowers/surprise/Her.webp',
-                link:'/her'
+                img: '/Images/flowers/surprise/Her.webp',
+                link: '/her'
             },
             {
-                img:'/Images/flowers/surprise/Them.webp',
-                link:'/them'
+                img: '/Images/flowers/surprise/Them.webp',
+                link: '/them'
             }
+        ],
+
+        color: [
+            {
+                image: "/Images/flowers/color/Blue.webp",
+                link: "/blue",
+                title: "Blue",
+            },
+            {
+                image: "/Images/flowers/color/Cool.webp",
+                link: "/cool",
+                title: "Cool",
+            },
+            {
+                image: "/Images/flowers/color/Mixed.webp",
+                link: "/mixed",
+                title: "Mixed",
+            },
+            {
+                image: "/Images/flowers/color/Orange.webp",
+                link: "/orange",
+                title: "Orange",
+            },
+            {
+                image: "/Images/flowers/color/Pastel.webp",
+                link: "/pastel",
+                title: "Pastel",
+            },
+            {
+                image: "/Images/flowers/color/Peach.webp",
+                link: "/peach",
+                title: "Peach",
+            },
+            {
+                image: "/Images/flowers/color/Pink.webp",
+                link: "/pink",
+                title: "Pink",
+            },
+            {
+                image: "/Images/flowers/color/Purple.webp",
+                link: "/purple",
+                title: "Purple",
+            },
+            {
+                image: "/Images/flowers/color/Red.webp",
+                link: "/red",
+                title: "Red",
+            },
+            {
+                image: "/Images/flowers/color/Warm.webp",
+                link: "/warm",
+                title: "Warm",
+            },
+            {
+                image: "/Images/flowers/color/White.webp",
+                link: "/white",
+                title: "White",
+            },
+            {
+                image: "/Images/flowers/color/Yellow.webp",
+                link: "/yellow",
+                title: "Yellow",
+            },
         ]
+
 
     }
 
@@ -139,7 +203,7 @@ function Page() {
         },
     });
 
-    const [slideRef,slide] = useKeenSlider({
+    const [slideRef, slide] = useKeenSlider({
         loop: true,
         slides: {
             perView: 5, // Number of visible slides
@@ -148,6 +212,25 @@ function Page() {
         breakpoints: {
             "(max-width: 1024px)": {
                 slides: { perView: 5, spacing: 2 },
+            },
+            "(max-width: 768px)": {
+                slides: { perView: 2, spacing: 8 },
+            },
+            "(max-width: 480px)": {
+                slides: { perView: 1, spacing: 5 },
+            },
+        },
+    });
+
+    const [sliderrRef, sliderr] = useKeenSlider({
+        loop: true,
+        slides: {
+            perView: 10, // Number of visible slides
+            spacing: 5, // Space between slides
+        },
+        breakpoints: {
+            "(max-width: 1024px)": {
+                slides: { perView: 10, spacing: 2 },
             },
             "(max-width: 768px)": {
                 slides: { perView: 2, spacing: 8 },
@@ -216,7 +299,7 @@ function Page() {
                 </div>
                 <div className="grid grid-cols-6 gap-6 ">
                     {flowerData.range.map((item, index) => (
-                        <Link href={`/gift${item.link}`}>
+                        <Link href={`/flowers${item.link}`}>
                             <img key={index} src={item.image} className='w-full h-auto rounded-xl' />
                         </Link>
                     ))}
@@ -240,74 +323,122 @@ function Page() {
             </div>
 
             <div>
-            <div className="space-y-10 bg-[url('/Images/flowers/bg.webp')] py-12 rounded-xl px-5 md:px-12">
-                <div className="flex items-center justify-center space-x-4">
-                    <div className="w-16 h-px bg-[#7d8035] "></div>
-                    <h2 className="font-bold text-3xl text-center">Flowers for Every Occasion</h2>
-                    <div className="w-16 h-px bg-[#7d8035]"></div>
-                </div>
+                <div className="space-y-10 bg-[url('/Images/flowers/bg.webp')] py-12 rounded-xl px-5 md:px-12">
+                    <div className="flex items-center justify-center space-x-4">
+                        <div className="w-16 h-px bg-[#7d8035] "></div>
+                        <h2 className="font-bold text-3xl text-center">Flowers for Every Occasion</h2>
+                        <div className="w-16 h-px bg-[#7d8035]"></div>
+                    </div>
                     <div className="relative">
-                    <button
-                        className="absolute left-2 top-1/2 transform -translate-y-1/2 bg-white rounded-full p-2 shadow-md z-10 hover:bg-gray-200"
-                        onClick={() => slide?.current?.prev()}
-                    >
-                        &#x276E;
-                    </button>
-                    
-                <div ref={slideRef} className="keen-slider">
-                    {flowerData.Occasion.map((item, index) => (
-                        <div key={index} className="keen-slider__slide">
-                            <Link href={`/flowers${item.link}`}>
+                        <button
+                            className="absolute left-2 top-1/2 transform -translate-y-1/2 bg-white rounded-full p-2 shadow-md z-10 hover:bg-gray-200"
+                            onClick={() => slide?.current?.prev()}
+                        >
+                            &#x276E;
+                        </button>
+
+                        <div ref={slideRef} className="keen-slider">
+                            {flowerData.Occasion.map((item, index) => (
+                                <div key={index} className="keen-slider__slide">
+                                    <Link href={`/flowers${item.link}`}>
+                                        <Image
+                                            src={item.img}
+                                            height={500}
+                                            width={500}
+                                            className="w-full h-[200px] rounded-3xl rounded-br-[64px] rounded-tl-[64px]"
+                                            alt="title"
+                                        />
+                                    </Link>
+
+
+
+                                </div>
+                            ))}
+                        </div>
+
+                        {/* Right Arrow */}
+                        <button
+                            className="absolute right-2 top-1/2 transform -translate-y-1/2 bg-white rounded-full p-2 shadow-md z-10 hover:bg-gray-200"
+                            onClick={() => slide?.current?.next()}
+                        >
+                            &#x276F;
+                        </button>
+                    </div>
+                    <div>
+
+                    </div>
+
+                </div>
+
+                <div className="space-y-10 relative bg-[url('/Images/flowers/Background.webp')] py-20 rounded-xl px-5 md:px-12">
+                    {/* Title Section */}
+                    <div className="flex items-center justify-center space-x-4 relative z-10">
+                        <Image src="/Images/flowers/choose/spark.svg" height={500} width={500} className="w-10 h-10" alt="spark" />
+                        <h2 className="font-bold text-3xl text-center">Plan The Perfect Surprise</h2>
+                        <Image src="/Images/flowers/choose/spark.svg" height={500} width={500} className="w-10 h-10" alt="spark" />
+                    </div>
+
+                    {/* Grid Section */}
+                    <div className="grid grid-cols-3 gap-5 relative z-10">
+                        {flowerData.surprise.map((item, index) => (
+                            <Link href={`/flowers${item.link}`} key={index} className="group relative">
                                 <Image
                                     src={item.img}
                                     height={500}
                                     width={500}
-                                    className="w-full h-[200px] rounded-3xl rounded-br-[64px] rounded-tl-[64px]"
                                     alt="title"
+                                    className="w-[400px] transform transition-transform duration-300 hover:scale-105 rounded-md"
                                 />
                             </Link>
-                            
+                        ))}
+                    </div>
 
-                   
-                        </div>
-                    ))}
+                    {/* Background Image */}
+                    <img src="/Images/flowers/suprise-n.svg" className="absolute inset-0 z-0" alt="background" />
                 </div>
 
-                 {/* Right Arrow */}
-                 <button
-                        className="absolute right-2 top-1/2 transform -translate-y-1/2 bg-white rounded-full p-2 shadow-md z-10 hover:bg-gray-200"
-                        onClick={() => slide?.current?.next()}
+                <div className="relative bg-[url('/Images/flowers/bg.webp')] py-16">
+                <div className="flex items-center justify-center space-x-4 relative z-10 mb-6">
+                        <Image src="/Images/flowers/choose/spark.svg" height={500} width={500} className="w-10 h-10" alt="spark" />
+                        <h2 className="font-bold text-3xl text-center">Choose a Favourite Colour</h2>
+                        <Image src="/Images/flowers/choose/spark.svg" height={500} width={500} className="w-10 h-10" alt="spark" />
+                    </div>
+                    <button
+                        className="absolute left-2 top-1/2 transform -translate-y-1/2 bg-white rounded-full p-2 shadow-md z-10 hover:bg-gray-200"
+                        onClick={() => sliderr?.current?.prev()}
                     >
-                        &#x276F;
+                        &#x276E;
                     </button>
-                </div>
-                <div>
 
-                </div>
-
-            </div>
-
-            <div className="space-y-10 relative bg-[url('/Images/flowers/Background.webp')] py-12 rounded-xl px-5 md:px-12">
-            <div className="flex items-center justify-center space-x-4">
-                    <Image src='/Images/flowers/choose/spark.svg' height={500} width={500} className="w-10 h-10" alt="spark" />
-                    <h2 className="font-bold text-3xl text-center">Plan The Perfect Surprise</h2>
-                    <Image src='/Images/flowers/choose/spark.svg' height={500} width={500} className="w-10 h-10" alt="spark" />
-                </div>
-                <div className="grid grid-cols-3 gap-5">
-                    {flowerData.surprise.map((item,index)=>(
-                        <div key={index}>
-                            <Image src={item.img} height={500} width={500} alt="title" className="w-[400px]"/>
+                    <div ref={sliderrRef} className="keen-slider  rounded-xl px-5 md:px-12">
+                        {flowerData.color.map((item, index) => (
+                            <Link href={`/flowers${item.link}`} key={index} className="keen-slider__slide space-y-4">
+                                {/* Image */}
+                                <Image
+                                    src={item.image}
+                                    height={500}
+                                    width={500}
+                                    alt={item.title}
+                                    className="w-full h-auto rounded-lg"
+                                />
+                                {/* Title */}
+                                <span className="flex justify-center text-lg font-semibold">{item.title}</span>
+                            </Link>
+                        ))}
                         </div>
-                    ))}
+
+                        <button
+                            className="absolute right-2 top-1/2 transform -translate-y-1/2 bg-white rounded-full p-2 shadow-md z-10 hover:bg-gray-200"
+                            onClick={() => sliderr?.current?.next()}
+                        >
+                            &#x276F;
+                        </button>
+                    </div>
 
                 </div>
 
-            <img src="/Images/flowers/suprise-n.svg" className="absolute inset-0"/>      
             </div>
-            </div>
-
-        </div>
-    );
+            );
 }
 
-export default Page;
+            export default Page;
